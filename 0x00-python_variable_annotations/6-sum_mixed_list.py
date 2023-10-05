17ec155cc2f3623270d08ecc6c6f97322188d647
Biny19 <binalfew.tadele@gmail.com>
2023-10-05 06:42:45 -0700

    It's all about python variable annotations

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 """
-A function that takes a mixed list of integers and floats 
-and returns the sum of all the numbers in the list as float
+Contains a function that takes a mixed list of integers and
+floats and returns the sum of all the numbers in the list as float
 """
-from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-    """Takes a mixed list of integers and floats and returns the
-    sum of all the numbers in the list as float."""
+def sum_mixed_list(mxd_lst):
     return sum(mxd_lst)
