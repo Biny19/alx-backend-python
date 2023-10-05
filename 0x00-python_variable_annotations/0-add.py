05256622ac7b7ca564d2fa5600fad240697e39fc
Biny19 <binalfew.tadele@gmail.com>
2023-10-05 06:31:58 -0700

    It's all about python variable annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module contains a single function that adds two float numbers"""
+
+
+def add(a: float, b: float) -> float:
+    """ Add two float numbers together"""
+    return a + b
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""A function that returns a string representation of a float"""
+
+
+def to_str(n: float) -> str:
+    """
+       Returns a string representation of the float
+    """
+    return str(n)
